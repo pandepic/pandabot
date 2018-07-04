1020c2bbb3ccfdd4097fa01f1050b08c06a2e454
pandepic <39251913+pandepic@users.noreply.github.com>
2018-07-05 09:09:44 +1000

    #

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-var roomStateManager = require('roomstatemanager');
-var creepEngine = require('creepengine');
-
-module.exports.loop = function() {
-    
-    roomStateManager.run();
-    creepEngine.run();
-    
-}
